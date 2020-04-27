e5520e7c42cb0e07b0acf082fe29ad8b9a8d8f91
gopigithub <gopi.ece1984@gmail.com>
2020-04-26 22:07:45 -0400

    my first xamarin project

---
+++ FirstProject/FirstProject/FirstProject/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using FirstProject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstProject
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private MainPageVM mainVM;
+        public MainPage()
+        {
+            InitializeComponent();
+            mainVM = new MainPageVM();
+            this.BindingContext = mainVM;
+        }
+
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+
+            //label.Text = entry.Text;
+
+            mainVM.onButtonClicked();
+        }
+    }
+}
---
+++ FirstProject/FirstProject/FirstProject/ViewModel/MainPageVM.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FirstProject.ViewModel
+{
+    public class MainPageVM : INotifyPropertyChanged
+    {
+        public MainPageVM()
+        {
+           
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string _labelText;
+        public string labelText
+        {
+            get { return _labelText; }
+            set
+            {
+                _labelText = value;
+                OnPropertyChanged("labelText");
+            }
+        }
+
+        string _entryText;
+        public string entryText
+        {
+            get { return _entryText; }
+            set
+            {
+                _entryText = value;
+                OnPropertyChanged("entryText");
+            }
+        }
+
+        public void onButtonClicked()
+        {
+            labelText = entryText;
+        }
+
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
